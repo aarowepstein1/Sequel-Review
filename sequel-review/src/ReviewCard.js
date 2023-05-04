9db57c503b7ffe81a2d3f38636aecccbc2b586d4
Aarow Epstein <aarowepstein1@gmail.com>
2023-05-04 15:20:11 -0700

    completed project

--- sequel-review/src/ReviewCard.js
+++ sequel-review/src/ReviewCard.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function ReviewCard({movie}) {
+    const [showDetails, toShowDetails] = useState(false)
+
+    function handleClick() {
+        toShowDetails(!showDetails)
+    }
     return(
-        <li>
+        <li id="review">
           <div>
-            <img src={movie.image} alt={movie.title}/>
+            <img onClick={handleClick} src={movie.image} alt={movie.title}/>
             <span>{`${movie.score}/5`}</span>
           </div>
           <strong>{movie.title}</strong>
-          <div id="details">
+          {showDetails ? <div id="details">
             <p>{movie.synopsis}</p>
             <p>{movie.review}</p>
-          </div>
+          </div> : null}
         </li>
     )
 }
--- sequel-review/src/ReviewsContainer.js
+++ sequel-review/src/ReviewsContainer.js
@@ -4,6 +4,8 @@ import ReviewCard from "./ReviewCard";
 function ReviewsContainer({movies}) {
     return (
         <main>
+            <h1>Reviews</h1>
+            <p>Click on an image to see more details</p>
             <ul>
                 {movies.map((movie) => (<ReviewCard 
                 key={movie.id}
